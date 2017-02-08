b7acf2869a37effc1d5dbd2cbc6eb8a6dbc9ef58
sleeper <survivingknowledge@gmail.com>
2017-02-08 18:48:40 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,14 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    #we need to see if :artist_id is set and if artist_id is valid artist
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      #artist_id exists but is not valid
+      redirect_to artists_path, alert: "Artist not found."
+    else
+      #this would set artist_id to nil if not exists but still works in this instance
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -39,7 +46,23 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @nested = true
+      #find and validate the artist
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        #no artist is found
+        redirect_to artists_path, alert: "Artist not found."
+      else
+        #we limit scope to only songs by artist_id
+        @song = artist.songs.find_by(id: params[:id])
+        #redirect to artists/:artist_id/songs and alert if no song is found
+        redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
+      end
+    else
+      #no artist_id so we are /songs/:id/edit
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +87,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
